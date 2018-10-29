30b063291f9397a022ff3c47103c8a775ec889ea
Valentyn Khoroshchak <smartgtsua@gmail.com>
2018-10-29 23:40:14 +0200

    Add characteristics functions

--- tims.py
+++ tims.py
@@ -1,6 +1,7 @@
 import collections
 import os.path
 import random
+from scipy import stats
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -23,7 +24,7 @@ def read_data(file_name):
     return _data
 
 
-def print_w_hist(data):
+def print_hist(data):
     data_len = len(data)
     plt.title('Histogram')
     plt.hist(
@@ -84,7 +85,7 @@ def calc_emp_disc(data):
 
 def get_r(n):
     i = 0
-    while not (pow(2, i) < n and (n <= pow(2, i + 1))):
+    while (not (pow(2, i) < n and (n <= pow(2, i + 1)))):
         i += 1
     return i
 
@@ -92,7 +93,7 @@ def get_r(n):
 def get_m(i, r, data):
     res = 0
     for item in data:
-        if (r + 1) * (i - 1) < item <= (r + 1) * i:
+        if ((r + 1) * (i - 1) < item and item <= (r + 1) * i):
             res += 1
     return res
 
@@ -106,6 +107,48 @@ def get_x_emp(r, n, f, data):
 
 def get_p(r):
     return 1 / (r + 1)
+
+
+def make_cont_table(data,amount):
+    step =get_r(amount)+1
+    res = []
+    i = 0
+    while(i<len(data)):
+        res.append(data[i:i+step])
+        i+=step
+    return res
+
+def make_count_avg_values(data):
+    res=[]
+    for i in data:
+        res.append(np.mean(i))
+    return res
+
+def numerical_characteristics(data):
+    data=np.array(data)
+    mode = stats.mode(data)
+    print('Мода: ', int(mode[0]))
+    mean = data.mean()
+    print('Середнє значення: ', mean)
+    median = np.median(data)
+    print('Медіана: ', median)
+    variansa = data.var()
+    print('Варіанса', variansa)
+    range = data.max() - data.min()
+    print('Розмах: ', range)
+    variatsiya = (variansa ** 0.5) / mean
+    print('Варіація: ', variatsiya)
+    quantil = np.quantile(data,q=[0.25, 0.5, 0.75])
+    de = np.quantile(data,q=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+    IDR = de[8] - de[0]
+    print('Інтердецильна широта', IDR)
+    IQR = quantil[2] - quantil[0]
+    print('Інтерквантильна широта', IQR)
+    asm = stats.skew(data)
+    print('Асиметрія: ', asm)
+    eks = stats.kurtosis(data)
+    print('Ексцес', eks)
+
 
 
 def main():
@@ -128,5 +171,15 @@ def main():
 
     # print_freq_plot(freq_table.keys(),freq_table.values())
 
+    arr =make_cont_table(data,amount)
+    arr=np.array(arr)
+    #print(arr)
+    res = make_count_avg_values(arr)
+    #print(res)
+    print("Дистретні \n")
+    numerical_characteristics(data)
+    print()
+    print("Неперервні \n")
+    numerical_characteristics(res)
 
 main()
